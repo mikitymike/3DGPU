c697f6762a76d41867c73cb65fe180ddaf356ac4
Zach Miller <zjmiller@purdue.edu>
2016-04-23 12:41:18 -0400

    changed wireframe to use 2 buffers, one for reading and one for writing

--- source/wireframe_sram.sv
+++ source/wireframe_sram.sv
@@ -11,20 +11,41 @@ import defines_package::*;
 
 module wireframe_sram
 (
-	input wire clk;
-	input wire write_en;
-	input wire data_in;
-	input wire [(WIREFRAME_ADDR_SIZE-1):0] addr;
-	output reg data_out;
+	input wire clk,
+	input wire n_rst,
+	input wire write_en,
+	input wire data_in,
+	input wire flip,
+	input wire [(WIREFRAME_ADDR_SIZE-1):0] write_addr,
+	input wire [(WIREFRAME_ADDR_SIZE-1):0] read_addr
+	output reg data_out
 );
 
-reg ram[0:((WIDTH+1)*HEIGHT)];
+reg write_buffer;
+reg ram0[0:((WIDTH+1)*HEIGHT)];
+reg ram1[0:((WIDTH+1)*HEIGHT)];
 
-always @(posedge clk) begin
-	if(write_en) begin
-		ram[addr] <= data_in;
+always_ff @(posedge clk, negedge n_rst) begin
+	if(n_rst) begin
+		write_buffer <= 0;
 	end
-	data_out <= ram[addr];
-end
+	else if(write_en) begin
+		if(write_buffer == 0) begin
+			ram0[write_addr] <= data_in;
+		end
+		else begin
+			ram1[write_addr] <= data_in;
+		end
+	end
 
-endmodule
+	if(write_buffer == 0) begin
+		data_out <= ram1[read_addr];
+	end
+	else begin
+		data_out <= ram0[read_addr];
+	end
+
+	if(flip) begin
+		write_buffer <= ~write_buffer;
+	end
+end
